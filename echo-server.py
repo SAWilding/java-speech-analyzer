923eb0e9fca5505e94613172fc7d1187cb47c3d7
Seth Wilding <sawilding7@gmail.com>
2022-11-16 17:49:26 -0700

    first echo server client

---
+++ echo-server.py
@@ -0,0 +1,77 @@
+import socket
+import sys
+import selectors
+import types
+# Primary socket methods
+#  socket()
+# .bind()
+# .listen()
+# .accept()
+# .connect()
+# .connect_ex()
+# .send()
+# .recv()
+# .close()
+
+
+sel = selectors.DefaultSelector()
+
+host, port = sys.argv[1], int(sys.argv[2])
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+print(f"Listening on {(host, port)}")
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)
+            data.outb = data.outb[sent:]
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+#     # socket.socket creates a socket object
+#     # AF_INET is for IPv4
+#     # SOCK_STREAM is for TCP 
+#     sock.bind((HOST, PORT)) # associates the socket with a specific network interface and port number
+#     sock.listen() # Makes the server a listening socket
+#     conn, addr = sock.accept() # Blocks execution and waits for incoming connection
+#     with conn:
+#         print(f"Connected by {addr}")
+#         while True:
+#             data = conn.recv(1024)
+#             if not data:
+#                 break
+#             conn.sendall(data)
